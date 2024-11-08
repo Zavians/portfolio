9178593c2f6109adeab6435a8c93ae4b34947461
KenzaVianda <wgolzqy5vzsp3todn1f664c9xc1mlj@bots.bitbucket.org>
2024-11-08 13:03:54 +0700

    Minor FIx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zavians Portofolio",
+  title: "Vianda Portofolio",
   description: "Modern & Minimal JS Mastery Portfolio",
 };
 
